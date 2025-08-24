106f384150c94e9952701289b715263e1bf9fba5
Shreejith-19 <shreejith1919@gmail.com>
2025-08-24 09:58:35 +0530

    feat: add connectDB() for establishing DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from "express"
 import notesRouter from "./src/routes/notesRoute.js"
+import { connectDB } from "./src/config/connectDB.js"
 const app = express()
 const port = 5000
+
+connectDB()
 
 app.use("/api/notes", notesRouter)
 
 app.listen(port, ()=>{
     console.log(`app listening at port ${port}`)
 })
+
+// 
