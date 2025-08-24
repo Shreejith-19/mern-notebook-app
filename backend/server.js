287593bac7a854190fd4bad99130d7e267a2d0bf
Shreejith-19 <shreejith1919@gmail.com>
2025-08-24 08:54:11 +0530

    refactor: add app.use for making the code concise

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express"
+import notesRouter from "./routes/notesRoute.js"
+const app = express()
+const port = 5000
+
+app.use("/api/notes", notesRouter)
+
+app.listen(port, ()=>{
+    console.log(`app listening at port ${port}`)
+})
