2d4657b35de1099d3ce4b245d22be453ef4f0d32
Shreejith-19 <shreejith1919@gmail.com>
2025-08-25 21:10:02 +0530

    chore: cleaning code

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
+
 import notesRouter from "./src/routes/notesRoute.js"
 import { connectDB } from "./src/config/connectDB.js"
 
 const app = express()
 const port = 5000
-
 dotenv.config()
 
 connectDB()
