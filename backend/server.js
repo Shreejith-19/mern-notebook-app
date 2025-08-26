03b28ab4b2d1c6d6d14baf47ef585c7e3a626377
Shreejith-19 <shreejith1919@gmail.com>
2025-08-26 21:17:12 +0530

    feat: add express.json and urlencoded middleware fro parsing request body

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv"
 
 import notesRouter from "./src/routes/notesRoute.js"
 import { connectDB } from "./src/config/connectDB.js"
+dotenv.config()
 
 const app = express()
-const port = 5000
-dotenv.config()
+const port = process.env.PORT || 5000
+//middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 connectDB()
 
