8a7f14640b039ab0f41c0b36b929733a830e305d
Shreejith-19 <shreejith1919@gmail.com>
2025-08-26 21:18:28 +0530

    feat: write code for controllers getNote and createNotes

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,9 +1,27 @@
+import Note from "../models/Note.js"
 export async function getNotes(req, res){
-    return res.status(200).send("You got the notes")
+    try{
+        const notes = await Note.find()//this will fetch all notes in db
+        return res.status(200).send(notes)
+    }catch(err){
+        console.log(`Error in getNodes Controller: ${err}`)
+        return res.status(500).json({"message": "Internal server Error"})
+    }
 }
 
 export async function createNotes(req, res){
-    return res.status(201).json({"meassage":"Notes created"})
+    try{
+        const body = req.body
+        const newNote = new Note({
+            title: body.title,
+            content: body.content
+        })
+        await newNote.save()
+        return res.status(201).json({"message":"new Note was created"})
+    }catch(err){
+        console.error(`error in createNotes controller: ${err}`)
+        return res.status(400).json({"message": "Bad request"})
+    }
 }
 
 export async function updateNotes(req, res){
