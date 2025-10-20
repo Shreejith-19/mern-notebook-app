3a7b4e26274af7d6cde5b9884d73188b303cb23e
Shreejith-19 <shreejith1919@gmail.com>
2025-10-20 12:33:56 +0530

    feat: Add routing and configure Toaster

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import HomePage from './pages/HomePage.jsx'
+import CreatePage from './pages/CreatePage.jsx'
+import NoteDetailPage from './pages/NoteDetailPage.jsx'
+import NoteFoundPage from './pages/NoteFoundPage.jsx'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+    errorElement: <NoteFoundPage/>,
+  },
+  {
+    path: "/create",
+    element: <CreatePage/>
+  },
+  {
+    path: "/notes/:noteId",
+    element: <NoteDetailPage/>
+  },
 
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
+    <Toaster/>
   </StrictMode>,
 )
