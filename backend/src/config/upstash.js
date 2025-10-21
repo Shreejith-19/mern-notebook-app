e49818fda2525bcb69f5d605035ff9434d59cead
Shreejith-19 <shreejith1919@gmail.com>
2025-10-21 16:49:30 +0530

    update ratelimit window

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -7,7 +7,7 @@ dotenv.config()
 const redis = Redis.fromEnv() //this will read directly from the .env
 const ratelimit = new Ratelimit({
     redis,
-    limiter : Ratelimit.slidingWindow(5, "20 s"),
+    limiter : Ratelimit.slidingWindow(60, "100 s"),
 })
  
 export default ratelimit
